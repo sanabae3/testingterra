c513a735af4c491b17979375e8e353fd99469fb3
sanabae3 <sanabae3@gmail.com>
2023-02-21 00:24:02 -0500

    adding all files

---
+++ modules/autoscaling/variables.tf
@@ -0,0 +1,29 @@
+variable "namespace" {
+  type = string
+}
+
+variable "aws_access_key_id" {
+  type = string
+}
+variable "aws_secret_access_key_id" {
+  type = string
+}
+variable "vpc" {
+  type = any
+}
+
+variable "sg" {
+  type = any
+}
+
+variable "db_config" {
+  type = object( #A
+    { #A
+      user     = string #A
+      password = string #A
+      database = string #A
+      hostname = string #A
+      port     = string #A
+    } #A
+  ) #A
+}
---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+namespace = "my-3-tier-architecture"
+region    = "us-east-1"
+
