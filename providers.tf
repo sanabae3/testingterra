80072aa256641341d1bf63d7384ee44bbd41b2f5
Serge <53587568+sanabae3@users.noreply.github.com>
2023-02-21 01:35:21 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,7 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 3.28"
+    }
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.1"
     }
   }
 }
