81a82eae9bf0b2ab7a0e7f68e4b3a1501a984a1e
Serge <53587568+sanabae3@users.noreply.github.com>
2023-02-22 07:22:56 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,16 +1,14 @@
+# https://registry.terraform.io/providers/hashicorp/aws/3.74.2
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.2.0"
-      
+      source = "hashicorp/aws"
+      version = "= 3.74.2"
     }
-    
   }
-  
 }
-
-# Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-west-2"
+  access_key = "AKIAQLO5FEV5LFXRKC5D"
+  secret_key = "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"
 }
